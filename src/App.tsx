93c6aa94bd372f2bc6acf86693c8df7aca341314
flopie <dehaan@dataprovider.com>
2022-12-15 09:02:45 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,156 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import "./App.css";
+import { Die } from "./components/Die/Die";
 
-function App() {
+const getDiceCoordinates = function (
+  n: number,
+  circleSize: number
+): [number, number][] {
+  if (n === 4) {
+    return [
+      [-70, -70],
+      [-70, 70],
+      [70, -70],
+      [70, 70],
+    ];
+  }
+
+  if (n === 2) {
+    return [
+      [0, -70],
+      [0, 70],
+    ];
+  }
+
+  if (n === 1) {
+    return [[0, 0]];
+  }
+
+  const theta: number[] = [];
+
+  const frags = 360 / n;
+  for (let i = 0; i <= n; i++) {
+    theta.push((frags / 180) * i * Math.PI);
+  }
+
+  console.log(theta);
+
+  const coordinates: [number, number][] = [];
+
+  for (let i2 = 0; i2 < n; i2++) {
+    coordinates.push([
+      Math.round(circleSize * Math.cos(theta[i2])),
+      Math.round(circleSize * Math.sin(theta[i2])),
+    ]);
+  }
+
+  return coordinates;
+};
+
+const App = () => {
+  const [diceLeft, setDiceLeft] = useState(5);
+  const [roll, setRoll] = useState(0);
+  const [coordinates, setCoordinates] = useState<[number, number][]>([]);
+  const [showFaces, setShowFaces] = useState(false);
+
+  useEffect(() => {
+    setCoordinates(getDiceCoordinates(diceLeft, diceLeft === 5 ? 115 : 80));
+  }, [diceLeft]);
+
+  useEffect(() => {
+    const documentHeight = () => {
+      const doc = document.documentElement;
+      doc.style.setProperty("--doc-height", `${window.innerHeight}px`);
+    };
+    window.addEventListener("resize", documentHeight);
+    documentHeight();
+  }, []);
+
+  const handleRoll = useCallback(() => {
+    setRoll(roll + 1);
+    setShowFaces(true);
+  }, [roll]);
+
+  const handleDecrement = useCallback(() => {
+    if (diceLeft > 0) {
+      setDiceLeft(diceLeft - 1);
+      setShowFaces(false);
+    }
+  }, [diceLeft]);
+
+  const handleIncrement = useCallback(() => {
+    if (diceLeft < 5) {
+      setDiceLeft(diceLeft + 1);
+      setShowFaces(false);
+    }
+  }, [diceLeft]);
+
+  const handleReset = useCallback(() => {
+    setDiceLeft(5);
+    setShowFaces(false);
+  }, []);
+
+  const canRoll = diceLeft > 0;
+  const canIncrement = diceLeft < 5 && diceLeft !== 0;
+  const canDecrement = diceLeft > 0;
+  const canReset = diceLeft < 5;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HelmetProvider>
+      <Helmet>
+        <title>Play Polliwop</title>
+        <meta
+          name="description"
+          content="Play Polliwop on your mobile phone!"
+        />
+        <meta
+          name="keywords"
+          content="play polliwop mobile phone online browser perudo"
+        />
+        <meta name="author" content="Floris de Haan" />
+        <link rel="canonical" href="https://solidesoftware.nl/polliwop/" />
+      </Helmet>
+      <div className="App">
+        <div className="main">
+          {diceLeft === 0 && <div className="game-over">Game over!</div>}
+          <div className="dice-container">
+            {coordinates.map((coordinate, index) => (
+              <Die
+                key={JSON.stringify(coordinate)}
+                roll={roll}
+                coordinate={coordinate}
+                animationLength={
+                  roll === 0 ? 0 : 1200 - diceLeft * 100 + index * 10
+                }
+                showFaces={showFaces}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="controls-wrapper">
+          <div className="controls">
+            <div className="controls-row">
+              <button onClick={handleRoll} disabled={!canRoll}>
+                Roll
+              </button>
+            </div>
+            <div className="controls-row">
+              <button onClick={handleIncrement} disabled={!canIncrement}>
+                +
+              </button>
+              <button onClick={handleReset} disabled={!canReset}>
+                Reset
+              </button>
+              <button onClick={handleDecrement} disabled={!canDecrement}>
+                -
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </HelmetProvider>
   );
-}
+};
 
 export default App;
---
+++ src/components/Die/Die.tsx
@@ -0,0 +1,112 @@
+import { useCallback, useEffect, useRef } from "react";
+import "./Die.css";
+
+export const Die = ({
+  roll,
+  coordinate,
+  animationLength,
+  showFaces,
+}: {
+  roll: number;
+  coordinate: [number, number];
+  animationLength: number;
+  showFaces: boolean;
+}) => {
+  const dieRef = useRef<HTMLDivElement>(null);
+
+  const x = useRef(0);
+  const z = useRef(0);
+
+  const handleRoll = useCallback(() => {
+    if (dieRef.current) {
+      const newX = Math.floor(Math.random() * 4) * 90;
+      const newZ = Math.floor(Math.random() * 4) * 90;
+
+      const finalX = newX === x.current ? newX - 360 : newX;
+      const finalZ = newZ === z.current ? newZ - 360 : newZ;
+
+      x.current = finalX;
+      z.current = finalZ;
+
+      dieRef.current.style.transform = `rotateX(${finalX + 0.1}deg) rotateZ(${
+        finalZ + 0.1
+      }deg)`;
+    }
+  }, []);
+
+  useEffect(() => {
+    handleRoll();
+  }, [handleRoll, roll]);
+
+  return (
+    <div
+      ref={dieRef}
+      className="die"
+      style={{
+        left: coordinate[0] - 40,
+        top: coordinate[1] - 40,
+        transition: `transform ${animationLength}ms`,
+      }}
+    >
+      <div id="die-side" className="side one">
+        {showFaces && (
+          <img
+            src="./polliwop.webp"
+            alt="polliwop"
+            style={{ width: 70, height: 70 }}
+          />
+        )}
+      </div>
+      <div id="die-side-two" className="side two">
+        {showFaces && (
+          <>
+            <div className="dot two-1"></div>
+            <div className="dot two-2"></div>
+          </>
+        )}
+      </div>
+      <div id="die-side-three" className="side three">
+        {showFaces && (
+          <>
+            <div className="dot three-1"></div>
+            <div className="dot three-2"></div>
+            <div className="dot three-3"></div>
+          </>
+        )}
+      </div>
+      <div id="die-side-four" className="side four">
+        {showFaces && (
+          <>
+            <div className="dot four-1"></div>
+            <div className="dot four-2"></div>
+            <div className="dot four-3"></div>
+            <div className="dot four-4"></div>
+          </>
+        )}
+      </div>
+      <div id="die-side-five" className="side five">
+        {showFaces && (
+          <>
+            <div className="dot five-1"></div>
+            <div className="dot five-2"></div>
+            <div className="dot five-3"></div>
+            <div className="dot five-4"></div>
+            <div className="dot five-5"></div>
+          </>
+        )}
+      </div>
+      <div id="die-side-six" className="side six">
+        {showFaces && (
+          <>
+            <div className="dot six-1"></div>
+            <div className="dot six-2"></div>
+            <div className="dot six-3"></div>
+            <div className="dot six-4"></div>
+            <div className="dot six-5"></div>
+            <div className="dot six-6"></div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
